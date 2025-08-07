37e95818a2a8ee5c76b74b2f17a850e875ce0487
Deep Sea <2286223728@qq.com>
2025-08-07 18:04:44 +0800

    增强token格式验证和移除不相关代码

--- server-api/middlewares/user-auth.js
+++ server-api/middlewares/user-auth.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const { User } = require("../models");
 const { UnauthorizedError } = require("../utils/errors");
-const { success, failure } = require("../utils/responses");
+const { failure } = require("../utils/responses");
 
 module.exports = async (req, res, next) => {
   try {
@@ -12,17 +12,41 @@ module.exports = async (req, res, next) => {
     }
 
     // 提取token
-    const token = authHeader.split(" ")[1];
-    if (!token) {
+    const tokenParts = authHeader.split(" ");
+    if (tokenParts[0] !== "Bearer" || !tokenParts[1]) {
       throw new UnauthorizedError("无效的授权头");
     }
 
+    const token = tokenParts[1];
+
     // 验证token是否有效
-    const decoded = jwt.verify(token, process.env.SECRET);
+    let decoded;
+    try {
+      decoded = jwt.verify(token, process.env.SECRET);
+    } catch (err) {
+      if (err instanceof jwt.TokenExpiredError) {
+        throw new UnauthorizedError("token已过期");
+      } else if (err instanceof jwt.JsonWebTokenError) {
+        throw new UnauthorizedError("无效的token");
+      } else {
+        throw new UnauthorizedError("token验证失败");
+      }
+    }
 
     // 查询用户是否存在
     const { userId } = decoded;
+    if (!userId) {
+      throw new UnauthorizedError("token信息不完整");
+    }
+
+    const user = await User.findByPk(userId);
+    if (!user) {
+      throw new UnauthorizedError("用户不存在");
+    }
+
+    // 将用户信息附加到请求对象
     req.userId = userId;
+    req.user = user;
     next();
   } catch (error) {
     failure(res, error);
