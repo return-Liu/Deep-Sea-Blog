f78723e520318f5b3cc449475ef83909b9aac31b
Deep Sea <2286223728@qq.com>
2025-08-29 10:26:42 +0800

    更新文件

--- server-api/routes/admin/upload.js
+++ server-api/routes/admin/upload.js
@@ -10,8 +10,8 @@ const userAuth = require("../../middlewares/user-auth");
 // OSS客户端配置
 const client = new OSS({
   region: "oss-cn-beijing",
-  accessKeyId: process.env.OSS_ACCESS_KEY_ID,
-  accessKeySecret: process.env.OSS_ACCESS_KEY_SECRET,
+  accessKeyId: LTAI5tC4zS7PyMMtvWbBpJWX,
+  accessKeySecret: Ztd6JU5cTH0XyUxS63QDRXvbmrNz4Q,
   authorizationV4: true,
   bucket: process.env.OSS_BUCKET,
 });
